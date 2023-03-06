2e7745730ce3c0f2520d5410d626cb94d2080bd6
Aleksis Boters 211RDB214 <125483680+aflexxxx@users.noreply.github.com>
2023-03-06 03:03:59 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -4,35 +4,55 @@ import sys
 import threading
 
 
+# python3
+
+import sys
+import threading
+
+
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    while n > 0:
-        max_height += 1
-        parents //= n
-    return max_height
+    tree = [[] for i in range(n)]
+    root = -1
+    for i in range(n):
+        if parents[i] == -1:
+            root = i
+        else:
+            tree[parents[i]].append(i)
+
+    def height(node):
+        if not tree[node]:
+            return 1
+        else:
+            return 1 + max([height(child) for child in tree[node]])
+
+    return height(root)
+
 
 def main():
-    # implement input form keyboard and from files
-    # Accept int input for user input
-    n = str(input)
-    parents = int(input)
-    max_height = compute_height(n, parents)
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    #with open("./directory/filename", mode="r"): as fails:
-            #text = fails.read() 
-    #print(text)
-    # input number of elements
-    print(max_height)
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
+    # take input from the user
+    mode = input("Enter 'F' to read from file, 'I' to read from keyboard: ")
+    while mode not in ['F', 'I']:
+        print("Invalid input!")
+        mode = input("Enter 'F' to read from file, 'I' to read from keyboard: ")
+
+    if mode == 'F':
+        # read input from file
+        file_name = input("Enter file name: ")
+        while 'a' in file_name:
+            print("File name cannot contain 'a'!")
+            file_name = input("Enter file name: ")
+        with open(f'folder/{file_name}', 'r') as f:
+            n = int(f.readline())
+            parents = list(map(int, f.readline().split()))
+    else:
+        # read input from keyboard
+        n = int(input())
+        parents = list(map(int, input().split()))
+
+    # compute tree height and print the result
+    print(compute_height(n, parents))
 
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+sys.setrecursionlimit(10**7) 
+threading.stack_size(2**27)   
 threading.Thread(target=main).start()
